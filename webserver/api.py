5c3d045188038daaf028e6de6aba50842c0ccee6
Danny Orlando <dorlando@colgate.edu>
2024-04-10 17:19:06 -0400

    Core Websocket Setup

--- webserver/api.py
+++ webserver/api.py
@@ -1,7 +1,29 @@
-from fastapi import (Request, FastAPI)
+from fastapi import (Request, FastAPI, WebSocket)
 
 app = FastAPI()
 
-@app.get('/whoami')
+
+
+# WhoAmI will be in charge of giving the player a cookie
+@app.get("/whoami")
 def whoami(request : Request):
     return "I am me."
+
+
+# This is going to be core websocket of maintaining player state
+@app.websocket("/netraiders")
+async def netraiders(websocket : WebSocket):
+    await websocket.accept()
+    iterator = 0
+    try:
+        while True:
+            _ = (await websocket.receive()).get("text", "")
+            iterator += 1
+            await websocket.send_text(f"ACK: {iterator}")
+    except Exception as e:
+        try:
+            await websocket.close()
+        except Exception as e:
+            logging.error(e)
+    finally:
+        ...
