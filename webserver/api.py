2fdbf1b37e02ca5641ade91262be2f52554f8c99
Danny Orlando <dorlando@colgate.edu>
2024-04-10 17:36:18 -0400

    Added Simple Protobuffs

--- webserver/api.py
+++ webserver/api.py
@@ -1,20 +1,21 @@
 from fastapi import (Request, FastAPI, WebSocket)
+from .proto_files import models_pb2
+import base64
 
 app = FastAPI()
-
-
 
 # WhoAmI will be in charge of giving the player a cookie
 @app.get("/whoami")
 def whoami(request : Request):
-    return "I am me."
-
+    player = models_pb2.PlayerModel(name="PhotosynthesisDev", score=420)
+    player_bytes = player.SerializeToString()
+    player_base64 = base64.b64encode(player_bytes).decode('utf-8')
+    return {"player": player_base64}
 
 # This is going to be core websocket of maintaining player state
 @app.websocket("/netraiders")
 async def netraiders(websocket : WebSocket):
     await websocket.accept()
-    iterator = 0
     try:
         while True:
             _ = (await websocket.receive()).get("text", "")
