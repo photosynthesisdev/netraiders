0274a54e29b4e5406acede6e6bc465b714c8804e
Danny Orlando <dorlando@colgate.edu>
2024-04-25 12:35:19 -0400

    Added Server Simulation Timestep

--- webserver/api.py
+++ webserver/api.py
@@ -3,38 +3,59 @@ from .proto_files import models_pb2
 import base64
 import logging
 import json
+import etcd3
+import time
 
 app = FastAPI()
-
 
 @app.get("/whoami")
 def whoami(request : Request):
     '''This endpoint will be in charge of issuing cookie to player for identifying them. Let them make persistent account, maintain leaderboard.'''
-    return "I am me."
+    return {"user": "BasicUser"}
 
 @app.websocket("/netraiderConnect")
 async def netraider(websocket : WebSocket):
-    from .models import NetRaiderPlayer, NetRaiderInput
+    from .models import NetRaiderPlayer, NetRaiderInput, ServerSimulation
     await websocket.accept()
+    # create a simulation object (the game room)
+    simulation = ServerSimulation()
+    # create our player object
     player = NetRaiderPlayer(user_id = 1, username = "BasicUser")
+    # create our database connection. We use ETCD for fast reliable key value store.
+    database = etcd3.client(host='localhost', port=2379)
     try:
-        # websocket's while true loop.
         while True:
-            # send the players current 'state' to them. When game starts, this will just be default values (score is zero). 
-            await websocket.send_text(player.json())
-            # receive from the player their inputs (pressed WASD, space, clicked)
-            raw_input_json = (await websocket.receive()).get("text", "") 
-            # player inputs
+            now_unix = time.time()
+            if now_unix > (simulation.last_time_step + (1/simulation.time_step)):
+                # Read all of a user's inputs from database. 
+                raw_database_tuples = database.get_prefix(f'/queued_inputs/basicuser')
+                # A 'network iteration' has passed. Update player of all other players states.
+                inputs = [json.loads(_tuple[0].decode()) for _tuple in raw_database_tuples]
+                # sort all inputs by order they were received in
+                inputs = sorted(inputs, key=lambda x: x['seq_num'])
+                # Initialize movement deltas
+                dx, dy = 0, 0
+                # user may have many inputs. Normalize them.
+                input_time_fraction = 0.1 / len(inputs) if inputs else 0
+                # Process all inputs for the tick
+                for player_input in inputs:
+                    if player_input['up']:
+                        dy += player.speed * input_time_fraction
+                    if player_input['down']:
+                        dy -= player.speed * input_time_fraction
+                    if player_input['left']:
+                        dx -= player.speed * input_time_fraction
+                    if player_input['right']:
+                        dx += player.speed * input_time_fraction
+                player.x += dx
+                player.y += dy
+                simulation.last_time_step = now_unix
+                # delete knowledge of inputs
+                database.delete_prefix(f'/queued_inputs/basicuser')
+                await websocket.send_text(player.json())
+            raw_input_json = (await websocket.receive()).get("text", "")
             player_inputs = json.loads(raw_input_json)
-            # PROCESS INPUTS
-            if player_inputs['up']:
-                player.y += 15
-            if player_inputs['down']:
-                player.y -= 15
-            if player_inputs['left']:
-                player.x -= 15
-            if player_inputs['right']:
-                player.x += 15
+            database.put(f'/queued_inputs/basicuser/{player_inputs.seq_num}', value=player_inputs.json())
     except Exception as e:
         try:
             logging.error(f'WebSocket Close --- {e}')
@@ -44,53 +65,3 @@ async def netraider(websocket : WebSocket):
     finally:
         # any cleanup logic we need goes here (such as notifying other players of this players disconnect, closing database connection, etc.)
         logging.error(f'WebSocket Finally closed')
-    
-
-@app.websocket("/netraidersProtobuf")
-async def netraidersProtobuf(websocket : WebSocket):
-    '''Websocket for testing Protobuf Serialization/Deserialization speed.'''
-    await websocket.accept()
-    # use the protobuf that we build in proto_files folder for PlayerModel (super sipmle, just name & score)
-    player = models_pb2.PlayerModel(name="Basic Client", score=0)
-    try:
-        # websocket's while true loop.
-        while True:
-            # send the players current 'state' to them. When game starts, this will just be default values (score is zero). 
-            await websocket.send_text(player.SerializeToString())
-            # receive from the player their inputs (pressed WASD, space, clicked)
-            inputs_bytes = await websocket.receive_bytes()
-            inputs = models_pb2.PlayerInputs().FromString(inputs_bytes)
-            # if player was 'pressing a key' add 1 to their score. In the current unity build, this is just true by default.
-            # However, in the actual game build, this is where any arbitrary logic would go where we would process the players inputs to compute their new 'state'
-            if inputs.pressed_key:
-                player.score += 1
-    except Exception as e:
-        try:
-            await websocket.close()
-        except Exception as e:
-            logging.error(e)
-    finally:
-        # any cleanup logic we need goes here (such as notifying other players of this players disconnect, closing database connection, etc.)
-        ...
-
-@app.websocket("/netraidersJson")
-async def netraidersJson(websocket : WebSocket):
-    '''Websocket for testing JSON speed. See Protobuf socket for comments on how that works'''
-    from .models import PlayerModel, PlayerInputs
-    await websocket.accept()
-    player = PlayerModel(name="Json Client", score = 0)
-    try:
-        while True:
-            await websocket.send_text(player.json())
-            input_str = (await websocket.receive()).get("text", "") 
-            #inputs = PlayerInputs.parse_obj(json.loads(input_str))
-            #if inputs.pressed_key:
-            player.score += 1
-    except Exception as e:
-        try:
-            logging.debug(e)
-            await websocket.close()
-        except Exception as e:
-            logging.debug(e)
-    finally:
-        ...
--- webserver/models.py
+++ webserver/models.py
@@ -1,16 +1,7 @@
 from pydantic import BaseModel
 
-
-class PlayerModel(BaseModel):
-    name : str
-    score : int
-
-class PlayerInputs(BaseModel):
-    player_name : str
-    pressed_key : bool
-
-
 class NetRaiderInput(BaseModel):
+    seq_num : int = 0
     up : bool
     down : bool
     left : bool
@@ -19,7 +10,11 @@ class NetRaiderInput(BaseModel):
 class NetRaiderPlayer(BaseModel):
     user_id : int
     username : str
+    speed : float = 10
     x : float = 0
     y : float = 0
     z : float = 0
-    
+
+class ServerSimulation(BaseModel):
+    time_step : int = 10 # 10 iterations per second
+    last_time_step : float = 0
